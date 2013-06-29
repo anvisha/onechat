39b29ac3110b34b1538d70dc253081a048f438ce
anvisha <anvisha@gmail.com>
2013-06-29 15:58:45 -0700

    hi

--- client/onechat.js
+++ client/onechat.js
@@ -2,13 +2,25 @@ Template.world.partier_id = function() {
 	return Session.get('partier_id');
 }
 
+Template.world.partiers = function() {
+	var partiers = Partiers.find().fetch();
+	var partier_names = [];
+	for(Object party : partiers){
+		partier_names.push(party.username);
+	}
+	return partier_names;
+}
+
 Meteor.startup(function() {
 	//when client opens
-	var partier_id = Partiers.insert({'username': null ,
+	var partier_id = Partiers.insert({'username': 'hi' ,
 		'x': null,
 		'y':null,
 		'video_stream': null });
 	Session.set('partier_id', partier_id);
+
+	var partiers = Partiers.find().fetch();
+
 
 });
 
